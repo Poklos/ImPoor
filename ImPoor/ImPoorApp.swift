d27923666c72412987e4295667ecf46ea390dc8d
Filip Pokłosiewicz <filip@kopiujwklej.net>
2024-08-02 11:03:39 +0200

    Initial Commit

---
+++ ImPoor/ImPoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImPoorApp.swift
+//  ImPoor
+//
+//  Created by Filip Pokłosiewicz on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImPoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
